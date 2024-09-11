6e543761ded42ab2f33512b8cc4cfa9c24685edf
sample <sample@gmail.com>
2024-09-11 21:59:17 +0530

    first commit

---
+++ remote.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "user authentication"
+read -p "enter username " username
+echo "username "
+read -p "enter passwd " passwd
+echo "passwd "
+if [ $username = "admin" -a  $passwd = "Administrator123" ]
+then
+        echo "authentication is sucessful "
+else
+        echo "authenticaton is unsucessful "
+fi
+        read -p "enter the character" char
+        if [ $char == "a" ] // [ $char == "e" ] // [ $char == "i" ] // [ $char == "o" ] // [ $char == "u" ]
+        then
+                echo "character is not vowels "
+        else
+                echo "character is vowels "
+        fi
+        echo "gender age"
+        read -p "enter the age " age
+        if [ $age -lt 13 ]
+        then
+                echo  "child "
+        elif [ $age -ge 13 -a $age -le 19 ]
+        then
+                echo " teenager "
+        else [ $age -gt 20 ]
+                echo "adult "
+
